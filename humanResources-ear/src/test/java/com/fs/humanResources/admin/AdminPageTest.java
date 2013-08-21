996c62d458cd3a8a6439a3c72ac15fbe6bfdfd6d
jaybono30@hotmail.com <jaybono30@hotmail.com>
2013-08-21 23:36:23 +0100

    Added Basic Selenium Tests

--- humanResources-ear/src/test/java/com/fs/humanResources/admin/AdminPageTest.java
+++ humanResources-ear/src/test/java/com/fs/humanResources/admin/AdminPageTest.java
@@ -1,12 +1,8 @@
 package com.fs.humanResources.admin;
 
 import com.fs.humanResources.common.BaseSeleniumTest;
-import com.fs.humanResources.common.SeleniumTestCategory;
-import org.junit.Assert;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 
-@Category(SeleniumTestCategory.class)
 public class AdminPageTest extends BaseSeleniumTest{
 
     @Test
--- humanResources-ear/src/test/java/com/fs/humanResources/common/SeleniumTestCategory.java
+++
@@ -1,4 +0,0 @@
-package com.fs.humanResources.common;
-
-public interface SeleniumTestCategory {
-}

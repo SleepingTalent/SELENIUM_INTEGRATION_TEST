19d6b93bd2b9a64be75c2425ad1f13bfa13643a7
jaybono30@hotmail.com <jaybono30@hotmail.com>
2013-09-10 23:21:10 +0100

    Removed redundant logging

--- humanResources-ear/src/test/java/com/fs/humanResources/common/BaseSeleniumTest.java
+++ humanResources-ear/src/test/java/com/fs/humanResources/common/BaseSeleniumTest.java
@@ -14,8 +14,6 @@ import java.util.Date;
 
 public abstract class BaseSeleniumTest {
 
-    private Logger log = Logger.getLogger(BaseSeleniumTest.class);
-
     protected static final String DATE_FORMATE_STR = "dd/MM/yyyy";
 
     private WebDriver driver;
@@ -32,7 +30,6 @@ public abstract class BaseSeleniumTest {
         driver = initialiseDriver();
         humanResourcesHome = new HumanResourcesHome(driver);
         persitenceHelper = new PersitenceHelper(entityManager);
-
         simpleDateFormat = new SimpleDateFormat(DATE_FORMATE_STR);
     }
 
@@ -42,17 +39,11 @@ public abstract class BaseSeleniumTest {
     }
 
     private WebDriver initialiseDriver() {
-        log.info("Running with FirefoxDriver");
         return new FirefoxDriver();
     }
 
     protected WebDriver getDriver() {
         return driver;
-    }
-
-    private boolean isDriverRun(String sysPropsDriverType, String expectedDriverType) {
-        log.info("System Property Driver Type : " + sysPropsDriverType);
-        return (sysPropsDriverType != null && !sysPropsDriverType.isEmpty() && sysPropsDriverType.equals(expectedDriverType));
     }
 
     protected String formateDate(Date date) {

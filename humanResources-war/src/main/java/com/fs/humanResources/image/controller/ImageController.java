c6ae09dc0d89b40cf3110d576885082eed22d4b1
jaybono30@hotmail.com <jaybono30@hotmail.com>
2013-09-26 20:35:34 +0100

    Added new Images and css to Index page.

--- humanResources-war/src/main/java/com/fs/humanResources/image/controller/ImageController.java
+++ humanResources-war/src/main/java/com/fs/humanResources/image/controller/ImageController.java
@@ -13,9 +13,21 @@ public class ImageController implements Serializable {
     public List<String> getNewsImages() {
 
         List<String> images = new ArrayList<String>();
-        images.add("desert-2.jpg");
-        images.add("lighthouse-2.jpg");
-        images.add("tulips-2.jpg");
+        images.add("office-1.jpg");
+        images.add("office-2.jpg");
+        images.add("office-3.jpg");
+        images.add("office-4.jpg");
+        images.add("office-5.jpg");
+
+        return images;
+    }
+
+    public List<String> getNewStartImages() {
+
+        List<String> images = new ArrayList<String>();
+        images.add("business-2.jpg");
+        images.add("business-3.jpg");
+        images.add("business-6.jpg");
 
         return images;
     }
--- humanResources-war/src/test/java/com/fs/humanResources/image/controller/ImageControllerTest.java
+++ humanResources-war/src/test/java/com/fs/humanResources/image/controller/ImageControllerTest.java
@@ -16,9 +16,11 @@ public class ImageControllerTest extends BaseUnitTest {
     public void getNewsImages_returnsExpectedImageNames() {
         List<String> imageNames = imageController.getNewsImages();
 
-        Assert.assertEquals(3, imageNames.size());
-        Assert.assertEquals("desert-2.jpg", imageNames.get(0));
-        Assert.assertEquals("lighthouse-2.jpg", imageNames.get(1));
-        Assert.assertEquals("tulips-2.jpg", imageNames.get(2));
+        Assert.assertEquals(5, imageNames.size());
+        Assert.assertEquals("office-1.jpg", imageNames.get(0));
+        Assert.assertEquals("office-2.jpg", imageNames.get(1));
+        Assert.assertEquals("office-3.jpg", imageNames.get(2));
+        Assert.assertEquals("office-4.jpg", imageNames.get(3));
+        Assert.assertEquals("office-5.jpg", imageNames.get(4));
     }
 }
